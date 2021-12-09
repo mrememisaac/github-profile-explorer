3cfaa209ecfd6eaee7f80865fe8bc2eafed76670
Emem Isaac <me@ememisaac.com>
2021-12-09 13:26:55 +0100

    Adds a header component

---
+++ src/Components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <button class="navbar-brand">GitHub Profile Explorer</button>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <button class="nav-link active" aria-current="page">Home</button>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  )
+}
